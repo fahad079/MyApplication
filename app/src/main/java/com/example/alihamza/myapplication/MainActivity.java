51121219eaf8e4cfb493df097b390e32080989d0
fahad079 <fahad079zaman@gmail.com>
2018-09-29 03:25:12 -0700

    up1

--- app/src/main/java/com/example/alihamza/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alihamza/myapplication/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.alihamza.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mCustomer, mDriver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //fahad
+
+        mDriver=(Button) findViewById(R.id.driver);
+        mCustomer=(Button) findViewById(R.id.customer);
+
     }
 }
